2c13d126e274f05dcd8f912de94c9888694bee99
dpikoppo <dave.pikop.pokam@ulb.be>
2023-11-11 14:39:48 +0100

    set Terminal and Nonterminal symbols and PMPGrammar.txt

---
+++ Part 2/src/NonTerminal.java
@@ -0,0 +1,17 @@
+public enum NonTerminal {
+    Program,
+    Code,
+    InstList,
+    Instruction,
+    For,
+    Assign,
+    ExprArith,
+    Op,
+    If,
+    Cond,
+    SimpleCond,
+    Comp,
+    While,
+    Print,
+    Read
+}
