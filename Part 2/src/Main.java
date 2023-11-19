c7a3f90cfe09dc0a20afc4579e5891efdd35846d
dpikoppo <dave.pikop.pokam@ulb.be>
2023-11-19 12:01:11 +0100

    set parseTree

--- Part 2/src/Main.java
+++ Part 2/src/Main.java
@@ -1,63 +1,72 @@
 import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.IOException;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.TreeMap;
-import java.util.List;
-import java.util.Map;
-
+import java.io.FileWriter;
+import java.util.Objects;
 
 public class Main {
+    /**
+     * Main
+     * @param argv
+     * @throws Exception
+     */
+    public static void main(String[] argv) throws Exception {
+        if (argv.length == 1 ) { // Print the rules
+            FileReader file;
+            try {
+                file = new FileReader(argv[0]);
+            }catch (Exception e){
+                throw new Exception(argv[0] + " file not found");
 
-    public static void main(String[] args) {
-        try {
-            FileReader file = new FileReader(args[0]);
+            }
             Parser parser = new Parser(file);
-            parser.program();
+            parser.program(); // parse the program
             Integer[] rules = parser.getRulesNumberList().toArray(new Integer[0]);
             for (Integer rule : rules) {
                 System.out.print(rule + " ");
             }
             System.out.println();
-//            FileReader grammarSource = new FileReader(args[args.length-1]);
-//            GrammarReader grammarReader = new GrammarReader(grammarSource);
-//            Grammar grammar = grammarReader.getGrammar();
-//            StringBuilder output = new StringBuilder();
-//            output.append("********** The Grammar **********\n");
-//            output.append(grammar);
-//
-//            grammar.setFirst();
-//            output.append("\n********** First **********\n" + grammar.stringFirst());
-//
-//            grammar.setFollow();
-//            output.append("\n********** Follow **********\n" + grammar.stringFollow());
-//
-////            grammar.setActionTable();
-////            output.append("\n********** Action table **********\n");
-//            System.out.print(output);
-//            // print start symbol
-//
-//            grammar.setActionTable();
-//            output.append("\n********** Action table **********\n");
-//            output.append(grammar.stringActionTable());
-//            System.out.println(output);
+        }
+        else if(argv.length == 3){  // Make the Parse Tree
+            if (Objects.equals(argv[0], "-wt")){
+                FileReader file;
+                try {
+                    file = new FileReader(argv[2]);
+                }catch (Exception e){
+                    throw new Exception(argv[2] + " file not found");
 
-//            // Print action table
-//            if (args.length > 0 && args[0].equals("-pat")) {
-//                System.out.println(grammar.stringActionTable());
-//            }
+                }
+                Parser parser = new Parser(file);
+                ParseTree parseTree = parser.program();
+                FileWriter TreeToTex=null;
+                try {
+                    TreeToTex = new FileWriter(argv[1]);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+                }catch (Exception e){
+                    throw new Exception("Cannot create the file : " + argv[1]);
+
+                }
+                TreeToTex.write(parseTree.toLaTeX());
+                TreeToTex.close();
+            }
+            else{
+                help();
+            }
+        }
+        else{
+            help();
+            return;
         }
     }
 
+    /**
+     * Help function to guide the users
+     */
+    private static void help(){
+        System.out.println("Usage : java -jar part2.jar [OPTION] [FILE]");
+        System.out.println("For print the parse tree (rules), the command for running your\n" +
+                "executable must be as follows:");
+        System.out.println("java -jar part2.jar sourceFile.fs");
+        System.out.println("For write the parse tree on tex file, the command for running your\n" +
+                "executable must be as follows: ");
+        System.out.println("java -jar part2.jar -wt sourceFile.tex sourceFile.fs");
+    }
 }
