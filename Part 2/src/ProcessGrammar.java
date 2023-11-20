7d1d3dd20b396df48de9a8c678ecda62a1f3f1be
dpikoppo <dave.pikop.pokam@ulb.be>
2023-11-20 14:51:52 +0100

    enabled exporting action table successfully

--- Part 2/src/ProcessGrammar.java
+++ Part 2/src/ProcessGrammar.java
@@ -1,17 +1,16 @@
 import java.io.FileReader;
 import java.io.FileNotFoundException;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.TreeMap;
-import java.util.List;
-import java.util.Map;
+
 public class ProcessGrammar {
     public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java ProcessGrammar <-wat|-pat> <output_file> <grammar_file>");
+            return;
+        }
+
         try {
-            FileReader grammarSource = new FileReader(args[args.length-1]);
+            FileReader grammarSource = new FileReader(args[args.length - 1]);
             GrammarReader grammarReader = new GrammarReader(grammarSource);
             Grammar grammar = grammarReader.getGrammar();
             StringBuilder output = new StringBuilder();
@@ -21,17 +20,21 @@ public class ProcessGrammar {
             output.append("\n********** First **********\n" + grammar.stringFirst());
             grammar.setFollow();
             output.append("\n********** Follow **********\n" + grammar.stringFollow());
-            // print start symbol
-
 
             grammar.setActionTable();
             output.append("\n********** Action table **********\n");
-            output.append(grammar.stringActionTable());
-            System.out.println(output);
-//            // Print action table
-//            if (args.length > 0 && args[0].equals("-pat")) {
-//                System.out.println(grammar.stringActionTable());
-//            }
+
+            if ("-wat".equals(args[0])) {
+                output.append(grammar.stringActionTable());
+                // Write action table to the specified file
+                FileManager.writeFile(args[1], output);
+            } else if ("-pat".equals(args[0])) {
+                // Print action table to the console
+                output.append(grammar.stringActionTable());
+                System.out.println(output);
+            } else {
+                System.out.println("Invalid option. Use <-wat|-pat>.");
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -39,5 +42,13 @@ public class ProcessGrammar {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+}
+
+class FileManager {
+    public static void writeFile(String filePath, StringBuilder content) throws IOException {
+        java.nio.file.Path path = java.nio.file.Paths.get(filePath);
+        java.nio.file.Files.write(path, content.toString().getBytes());
+        System.out.println("Action table written to " + filePath);
     }
 }
